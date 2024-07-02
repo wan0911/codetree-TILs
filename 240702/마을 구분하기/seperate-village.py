cb799cff6bc35264c015051299cfd60d1ce46203
wan <106522971+wan0911@users.noreply.github.com>
2024-07-02 22:27:21 +0900

    Title: 마을 구분하기, Time: 167ms, Memory: 28MB, Status: Passed - CodeTree

--- 240702/마을 구분하기/seperate-village.py
+++ 240702/마을 구분하기/seperate-village.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
@@ -7,21 +8,29 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0] * n for _ in range(n)]
 dx, dy = [0, 0, -1, 1], [1, -1, 0, 0]
 
-def dfs(x, y, cnt):
-    visited[x][y] = 1
+def bfs(a, b):
+    q = deque()
+    q.append((a, b))
+    cnt = 1
 
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1 and not visited[nx][ny]:
-            cnt = dfs(nx, ny, cnt + 1)
+    while q:
+        x, y = q.popleft()
+        visited[x][y] = 1
 
-    return cnt
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1 and not visited[nx][ny]:
+                q.append((nx, ny))
+                visited[nx][ny] = 1
+                cnt += 1
+    return cnt                 
 
 res = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1 and not visited[i][j]:
-            res.append(dfs(i, j, 1))
+            res.append(bfs(i, j))
 
 res.sort()
 print(len(res))
