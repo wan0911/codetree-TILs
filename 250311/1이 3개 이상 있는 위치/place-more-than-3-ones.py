6b94a38372ddd24f46b53f07ec7f6573fcf0c75f
wan <106522971+wan0911@users.noreply.github.com>
2025-03-11 21:01:27 +0900

    Title: 1이 3개 이상 있는 위치, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250311/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,17 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+dx, dy = [0, 0, -1, 1], [1, -1, 0, 0]
+result = 0
+
+for i in range(4):
+    for j in range(4):
+        cnt = 0
+        for k in range(4):
+            x, y = i + dx[k], j + dy[k]
+            if 0 <= x < n and 0 <= y < n and grid[x][y] == 1:
+                cnt += 1
+                if cnt >= 3: 
+                    result += 1 
+                    break
+
+print(result)
